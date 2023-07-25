3c75a22c13485faa05e61a594877ad7eb406ae0c
Dev Mittal <devmittal0407@gmail.com>
2023-07-25 15:36:29 +0530

    Added dat.gui

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
@@ -46,12 +47,36 @@ plane.rotation.x = -0.5 * Math.PI
 const gridHelper = new THREE.GridHelper(30)
 scene.add(gridHelper)
 
+// Dat GUI
+const gui = new dat.GUI()
+
+const options = {
+  sphereColor: '#ffea00',
+  wireframe: true,
+  speed: 0.01
+}
+
+gui.addColor(options, 'sphereColor').onChange(function (e) {
+  sphere.material.color.set(e)
+})
+
+gui.add(options, 'wireframe').onChange(function (e) {
+  sphere.material.wireframe = e
+})
+
+gui.add(options, 'speed', 0, 0.1)
+
+let step = 0
+
 // Animations for the elements go here
 function animate() {
   requestAnimationFrame(animate)
 
   cube.rotation.x += 0.01
   cube.rotation.y += 0.01
+
+  step += options.speed
+  sphere.position.y = 10 * Math.abs(Math.sin(step))
 
   renderer.render(scene, camera)
 }
