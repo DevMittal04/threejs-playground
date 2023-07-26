fccd92e78a0579183d7fc221b714afae3ca9c316
Dev Mittal <devmittal0407@gmail.com>
2023-07-27 00:08:22 +0530

    Added Spotlight and Fog

--- main.js
+++ main.js
@@ -60,17 +60,30 @@ const ambientLight = new THREE.AmbientLight(0x333333)
 scene.add(ambientLight)
 
 // Directional Light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8)
-scene.add(directionalLight)
-directionalLight.position.set(-30, 50, 0)
-directionalLight.castShadow = true
-directionalLight.shadow.camera.bottom = -12
+// const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8)
+// scene.add(directionalLight)
+// directionalLight.position.set(-30, 50, 0)
+// directionalLight.castShadow = true
+// directionalLight.shadow.camera.bottom = -12
 
-const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 5)
-scene.add(directionalLightHelper)
+// const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 5)
+// scene.add(directionalLightHelper)
 
-const directionalLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
-scene.add(directionalLightShadowHelper)
+// const directionalLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+// scene.add(directionalLightShadowHelper)
+
+const spotLight = new THREE.SpotLight(0xffffff)
+scene.add(spotLight)
+spotLight.position.set(-100, 100, 0)
+spotLight.castShadow = true
+spotLight.angle = 0.2
+spotLight.distance = 500
+
+const spotLighHelper = new THREE.SpotLightHelper(spotLight)
+scene.add(spotLighHelper)
+
+// scene.fog = new THREE.Fog(0xffffff, 50, 200)
+scene.fog = new THREE.FogExp2(0xffffff, 0.01)
 
 // Dat GUI
 const gui = new dat.GUI()
@@ -78,7 +91,10 @@ const gui = new dat.GUI()
 const options = {
   sphereColor: '#ffea00',
   wireframe: true,
-  speed: 0.01
+  speed: 0.01,
+  angle: 0.2,
+  penumbra: 0,
+  intensity: 1
 }
 
 gui.addColor(options, 'sphereColor').onChange(function (e) {
@@ -91,6 +107,10 @@ gui.add(options, 'wireframe').onChange(function (e) {
 
 gui.add(options, 'speed', 0, 0.1)
 
+gui.add(options, 'angle', 0, 1)
+gui.add(options, 'penumbra', 0, 1)
+gui.add(options, 'intensity', 0, 1)
+
 let step = 0
 
 // Animations for the elements go here
@@ -102,6 +122,11 @@ function animate() {
 
   step += options.speed
   sphere.position.y = 10 * Math.abs(Math.sin(step))
+
+  spotLight.angle = options.angle
+  spotLight.penumbra = options.penumbra
+  spotLight.intensity = options.intensity
+  spotLighHelper.update()
 
   renderer.render(scene, camera)
 }
